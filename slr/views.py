56f1a690d07a4dbd2012f3bf4eb0f07176fba675
Bhanu prathap <60665107+bhanuprathap2000@users.noreply.github.com>
2022-03-31 15:41:16 +0530

    Update views.py

--- slr/views.py
+++ slr/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .forms import UploadFileForm
 from django.views.decorators.csrf import ensure_csrf_cookie
 from SLRA.real_time_prediction import get_video_and_predict
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 @ensure_csrf_cookie
 def upload_display_video(request):
     if request.method == 'POST':
@@ -11,7 +16,7 @@ def upload_display_video(request):
             #print(file.name)
             handle_uploaded_file(file)
             #write the code to predict the sign in video
-            prediction_data=get_video_and_predict(f"D:/final year project/SLR/core/{file.name}")
+            prediction_data=get_video_and_predict(f"{BASE_DIR}/{file.name}")
 
             return render(request, "index.html", {'filename': file.name,"data":prediction_data[0]})
     else:
