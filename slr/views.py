f8c79b9d2002858c4be5493f39433208bb19dbe2
Bhanu prathap <60665107+bhanuprathap2000@users.noreply.github.com>
2022-03-31 12:00:08 +0530

    Update views.py

--- slr/views.py
+++ slr/views.py
@@ -11,7 +11,7 @@ def upload_display_video(request):
             #print(file.name)
             handle_uploaded_file(file)
             #write the code to predict the sign in video
-            prediction_data=model.predit
+            prediction_data=get_video_and_predict(f"D:/final year project/SLR/core/{file.name}")
 
             return render(request, "index.html", {'filename': file.name,"data":prediction_data[0]})
     else:
