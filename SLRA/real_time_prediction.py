eaf5a1bd07e8ad6834ce3eded655179b47d3a72d
Bhanu prathap <60665107+bhanuprathap2000@users.noreply.github.com>
2022-03-31 15:27:11 +0530

    Update real_time_prediction.py

--- SLRA/real_time_prediction.py
+++ SLRA/real_time_prediction.py
@@ -153,7 +153,7 @@ def get_video_and_predict(video_path):
     link="https://dl.dropboxusercontent.com/s/9e0sqm2077epi8i/transformer.pth"
 
     pretrained_model_name = "SLRA/trained_models/transformer.pth"
-    torch.hub.download_url_to_file(link,"/content/drive/MyDrive/INCLUDE/test.pth",pretrained_model_name, progress=True)
+    torch.hub.download_url_to_file(link,pretrained_model_name, progress=True)
 
     ckpt = torch.load(pretrained_model_name,map_location=torch.device('cpu'))
     model.load_state_dict(ckpt["model"])
