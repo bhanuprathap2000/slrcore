be7d6775a37eaec678290c847c35ea551ff75735
Bhanu prathap <60665107+bhanuprathap2000@users.noreply.github.com>
2022-03-31 10:47:31 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yg%qr-^554ur^00o$c%5v9*08-ao&(d6$pw#h@vy17vo7nopj8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://slrcore.herokuapp.com/','https://slrcore.herokuapp.com']
 
 
 # Application definition
